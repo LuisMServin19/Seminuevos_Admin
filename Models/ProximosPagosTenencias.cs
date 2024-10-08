6a9c9c75b0238fc6f8de630982db35ff96f9cd74
luisservin <luis.mercado.servin@gmail.com>
2024-10-07 21:31:24 -0600

    Se ajusta para realizar pago de tenencia

--- Models/ProximosPagosTenencias.cs
+++ Models/ProximosPagosTenencias.cs
@@ -8,38 +8,11 @@ namespace Serfitex.Models
         [Key]
 
         public int Id_unidad { get; set; }
-
         public string? Modelo { get; set; }
-
         public string? Marca { get; set; }
-
-        [DisplayName("Número de serie")]
-        public string? Num_serie { get; set; }
-
-        [DisplayName("Año")]
-        public string? Ano { get; set; }
-        public string? Color { get; set; }
-
-        [DisplayName("Fecha factura")]
-        public DateTime Fecha_factura { get; set; }
-
-        [DisplayName("Fecha tenencia")]
-        public DateTime Fecha_tenencia { get; set; }
-        
-        public string? Seguro { get; set; }
-        public string? Comentario { get; set; }
         public string? Sucursal { get; set; }
-        public int? Estatus { get; set; }
-        public string? EstatusTexto { get; set; }
-        
-        [DisplayName("Fecha ingreso")]
-        public DateTime Fecha_ingreso { get; set; }
-
-        [DisplayName("Proximo pago tenencia")]
-        public DateTime? Fech_prox_tenecia { get; set; }
-
-        [DisplayName("Proximo pago verificacion")]
-        public DateTime? Fech_prox_verificacion { get; set; }
+        public DateTime Fech_prox_tenecia { get; set; }
+        public DateTime Fecha_pago { get; set; }
 
     }
 }
